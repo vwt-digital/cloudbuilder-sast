ca1373ebcacdadc1c8d96c053eea965759df7809
David Strootman <david.strootman@gmail.com>
2020-02-25 15:12:57 +0100

    DAT-1990: Suppress Bandit subprocess warnings and flush print statements

--- test.py
+++ test.py
@@ -1,25 +1,25 @@
 import os
-import subprocess
+import subprocess  # nosec
 import unittest
 
 
 class TestSastScan(unittest.TestCase):
 
     def testPositive(self):
-        print("############### POSITIVE TESTS ###############")
+        print("############### POSITIVE TESTS ###############", flush=True)
         for file in os.listdir('tests/positive'):
             self.assertEqual(subprocess.call('./docker-sast.sh'
                                              ' --type python'
                                              ' --type typescript'
-                                             ' --target tests/positive/' + file, shell=True), 0)
+                                             ' --target tests/positive/' + file, shell=True), 0)  # nosec
 
     def testNegative(self):
-        print("############### NEGATIVE TESTS ###############")
+        print("############### NEGATIVE TESTS ###############", flush=True)
         for file in os.listdir('tests/negative'):
             self.assertEqual(subprocess.call('./docker-sast.sh'
                                              ' --type python'
                                              ' --type typescript'
-                                             ' --target tests/negative/' + file, shell=True), 1)
+                                             ' --target tests/negative/' + file, shell=True), 1)  # nosec
 
 
 if __name__ == '__main__':
